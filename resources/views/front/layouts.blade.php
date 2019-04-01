70abcc0d795589f5d0586a8541688db129b62d0d
harryrahmansyah97 <harryrahmansyah97@gmail.com>
2019-04-01 15:15:33 +0700

    ganti foto

--- resources/views/front/layouts.blade.php
+++ resources/views/front/layouts.blade.php
@@ -33,7 +33,7 @@
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('trending')}}">
         <div class="sidebar-brand-icon ">
-            <img src="{{ asset('img/logo.png')}}" width="60">
+            <img src="{{ asset('img/logo1.png')}}" width="60">
         </div>
       </a>
 
@@ -250,7 +250,7 @@
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                <img class="img-profile rounded-circle" src="{{ asset('img/test-1.png')}}">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
